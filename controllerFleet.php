b3b29406513efd03ce0c41a06f79ac61b9657b9b
noarr <rr.noar@gmail.com>
2018-07-17 11:56:11 +0200

    controllefeet

---
+++ controllerFleet.php
@@ -0,0 +1,22 @@
+<?php
+require_once'dbconnect.php';
+$link=$con;
+
+
+$sql = "SELECT car.IMG AS img_link, car.model AS 'model', car.latitude AS 'latitude', car.longitude AS 'longitude'
+				 FROM car ";
+
+$result = mysqli_query($link, $sql);
+$posts= array();
+//var_dump($row = mysqli_fetch_assoc($result));
+		while ($row = mysqli_fetch_assoc($result)) { 
+		$posts[] = $row;
+
+		
+
+			// echo $row['model'] 
+ 		// 	echo $row['latitude'] 
+			// echo $row['longitude'] 
+		}		    	
+		
+?>
